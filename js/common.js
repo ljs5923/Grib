13b7667600f613bb3f65e5d4ec22ced3e0c79da1
JinSu <ljs5923@naver.com>
2018-10-16 03:44:27 +0900

    grib

--- js/common.js
+++ js/common.js
@@ -1,17 +1,26 @@
 jQuery(function ($) {
     var $_gnbBtn = $('._gnb_btn');
-    var $_gnb_wrap = $('._gnb_wrap');
+    var $_gnbWrap = $('._gnb_wrap');
+    var $_contentMenu = $('._content_menu_lst');
     $_gnbBtn.on('click',function(){
         $(this).toggleClass('_on');
-        $_gnb_wrap.toggleClass('_on');
+        $_gnbWrap.toggleClass('_on');
     });
     var $_gnbLst = $('._gnb_lst');
     $_gnbLst.hover(
         function () {
-            $(this).find('._gnb_sub').stop().slideDown(300);
+            $(this).find('._gnb_sub').stop().slideDown(500);
         },
         function () {
-            $(this).find('._gnb_sub').stop().slideUp(300);
+            $(this).find('._gnb_sub').stop().slideUp(500);
+        }
+    );
+    $_contentMenu.hover(
+        function(){
+            $(this).addClass('active');
+        },
+        function(){
+            $(this).removeClass('active');
         }
     );
 })
